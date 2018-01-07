285e47f97cc747287b81e5009a040d02f4adab92
Alastair Danley-Cox <AliC@users.noreply.github.com>
2018-01-07 17:19:42 +0000

    Added a player repository Using TestServer for integration tests Added Empty web template for reference

--- src/FFL.Data.Tests/UnitTest1.cs
+++ src/FFL.Data.Tests/UnitTest1.cs
@@ -1,5 +1,14 @@
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Configuration;
+using Moq;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -10,12 +19,42 @@ namespace FFL.Data.Tests
         [Fact]
         public async Task Test1Async()
         {
-
-
-            PlayerRepository repo = new PlayerRepository();
+            var builder = foo();
+            TestServer server = new TestServer(builder);
+            HttpClient client = server.CreateClient();
+            PlayerRepository repo = new PlayerRepository(client);
             IList<PlayerProperties> playerStats = await repo.GetStatsAsync();
 
             Assert.NotNull(playerStats);
+            Assert.NotEmpty(playerStats);
+            Assert.Equal("Kevin De Bruyne", playerStats[0].first_name);
+        }
+
+        //public static IWebHost BuildWebHost(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
+
+        private IWebHostBuilder foo()
+        {
+            string[] args = new string[] { };
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .AddCommandLine(args)
+                
+                .Build();
+
+            return WebHost.CreateDefaultBuilder()
+                //.UseUrls("http://*:5000")
+                .UseConfiguration(config)
+                .Configure(app =>
+                {
+                    //app.Map(,)
+                    app.Run(context =>
+                        context.Response.WriteAsync("[{\"first_name\":\"Kevin De Bruyne\"}]"));
+                });
         }
     }
 }
---
+++ src/FFL.Data/PlayerRepository.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FFL.Data
+{
+    public class PlayerRepository
+    {
+        private HttpClient _httpClient;
+
+        public PlayerRepository(HttpClient client) => _httpClient = client;
+
+        public async Task<IList<PlayerProperties>> GetStatsAsync()
+        {
+            IList<PlayerProperties> playerProperties = new List<PlayerProperties>();
+
+            string uri = "https://fantasy.premierleague.com/drf/elements/";
+            using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri))
+            {
+                var response = await _httpClient.SendAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    playerProperties = JsonConvert.DeserializeObject<IList<PlayerProperties>>(responseContent);
+                }
+            }
+
+            return playerProperties;
+        }
+    }
+}
