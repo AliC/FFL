96813a3b19fdeff129f224552ba992313efe0c24
AlastairDanleyCox <alastair.danleycox@totaljobsgroup.com>
2018-01-09 08:21:44 +0000

    Added missing commit from last checkin

--- src/FFL.Data/PlayerRepository.cs
+++ src/FFL.Data/PlayerRepository.cs
@@ -14,20 +14,18 @@ namespace FFL.Data
 
         public async Task<IList<PlayerProperties>> GetStatsAsync()
         {
-            IList<PlayerProperties> playerProperties = new List<PlayerProperties>();
-
-            string uri = "https://fantasy.premierleague.com/drf/elements/";
-            using (HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri))
+            var uri = "https://fantasy.premierleague.com/drf/elements/";
+            using (var request = new HttpRequestMessage(HttpMethod.Get, uri))
             {
                 var response = await _httpClient.SendAsync(request);
                 if (response.IsSuccessStatusCode)
                 {
-                    string responseContent = await response.Content.ReadAsStringAsync();
-                    playerProperties = JsonConvert.DeserializeObject<IList<PlayerProperties>>(responseContent);
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<IList<PlayerProperties>>(responseContent);
                 }
             }
 
-            return playerProperties;
+            return new List<PlayerProperties>();
         }
     }
 }
