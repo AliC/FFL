fd2e6f4358cfce18a141ef6a1500d1b6717aa466
Alastair Danley-Cox <AliC@users.noreply.github.com>
2018-09-16 15:44:03 +0100

    Registered DI services

--- src/FFL.UI.Web.Mvc/Startup.cs
+++ src/FFL.UI.Web.Mvc/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using FFL.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -22,6 +24,8 @@ namespace FFL.UI.Web.Mvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton(new HttpClient());
+            services.AddTransient<IPlayerRepository, PlayerRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
